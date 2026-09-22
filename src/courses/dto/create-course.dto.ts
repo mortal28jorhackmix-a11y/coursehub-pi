8dac32fa522bb725742eb9feca3f447351a70706
Jordan Solórzano <jorscarmia28@gmail.com>
2026-09-22 14:07:52 -0500

    prueba

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -3,8 +3,8 @@ import { IsIn, IsNotEmpty, IsString } from 'class-validator'; // 1
 export class CreateCourseDto { // 2
   @IsString() // 3
   @IsNotEmpty() // 4
-  title: string;
+  title: string;     
 
   @IsIn(['beginner', 'intermediate', 'advanced']) // 5
   level: string;
-}
+} 
